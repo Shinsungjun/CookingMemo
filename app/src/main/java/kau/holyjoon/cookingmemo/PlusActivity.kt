836cb9418de31f58d1f115264c1ff54cb3c0d546
holy_joon <ssjun511@gmail.com>
2019-11-19 01:30:38 +0900

    재료화면 검색만 하면 끝

--- app/src/main/java/kau/holyjoon/cookingmemo/PlusActivity.kt
+++ app/src/main/java/kau/holyjoon/cookingmemo/PlusActivity.kt
@@ -14,8 +14,9 @@ import kotlinx.android.synthetic.main.plus_popup.*
 class PlusActivity :AppCompatActivity() {   //+ 버튼 클릭 시 나타나는 popup형식 Activity 여기서 만들어진 Recipe객체를 Intent를 통해 EditActivity로 넘겨준다. 그러면 EditActivity에서도 똑같이 객체화!
     //done 버튼을 누르면 지금까지 입력한 재료, 방법, 시간에 대한 정보를 가지고 조건에 맞는다면 Recipe객체를 만들어 Intent로 전달.
     //조건에 맞지 않는다면 무엇이 부족한지 알려줌.
-    var ingredients = ArrayList<Ingredient?>()
+    var ingredients = ArrayList<Ingredient?>()  // ingredients = arrayListOf<Ingredient>()
     val popupAdapter = Popup_Adapter(this, ingredients)
+    val ingre = arrayListOf<ArrayList<Ingredient>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
